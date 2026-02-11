32501e8893e8a1b140a25162f6f0bb998c8a9e33
Fredy Augusto Castellon Bedoya <castellonbedoya@gmail.com>
2026-02-11 10:20:35 -0600

    CH1: HTTP Streams - 8 Bytes

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main()  {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatalf("unable to open file: %v", err)
+	}
+	defer file.Close()
+
+	
+	for {
+		buffer := make([]byte, 8)
+		n, err := file.Read(buffer)
+
+		if err == io.EOF {
+			break
+		}
+		
+		if err != nil {
+			log.Fatalf("error reading file: %v", err)
+		}
+
+		if n > 0 {
+			fmt.Printf("read: %s\n", string(buffer))
+		}
+	}
+	
 }
