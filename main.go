662bbc0c64386d85a2935c688b7b14b84fa3c0b4
Fredy Augusto Castellon Bedoya <castellonbedoya@gmail.com>
2026-02-13 12:54:49 -0600

    CH1: HTTP Streams - Channel Refactor

--- main.go
+++ main.go
@@ -21,31 +21,48 @@ func main()  {
 	fmt.Printf("Reading data from %s\n", inputFilePath)
 	fmt.Println("=====================================")
 
-	currentLineContents := ""
-	for {
-		buffer := make([]byte, 8)
-		n, err := f.Read(buffer)
+	linesChan := getLinesChannel(f)
 
-		if err != nil {
-			if currentLineContents != "" {
-				fmt.Printf("read: %s\n", currentLineContents)
+	for line := range linesChan {
+		fmt.Println("read:", line)
+	}
+
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(lines)
+		currentLineContents := ""
+		for {
+			buffer := make([]byte, 8)
+			n, err := f.Read(buffer)
+
+			if err != nil {
+				if currentLineContents != "" {
+					lines <- currentLineContents
+					currentLineContents = ""
+				}
+
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				
+				fmt.Printf("error: %s\n", err.Error())
+				return
+			}
+
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
+			for i := 0; i < len(parts)-1; i++ {
+				lines <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
 				currentLineContents = ""
 			}
-
-			if errors.Is(err, io.EOF) {
-				break
-			}
-			
-			fmt.Printf("error: %s\n", err.Error())
-			break
+			currentLineContents += parts[len(parts)-1]
 		}
-
-		str := string(buffer[:n])
-		parts := strings.Split(str, "\n")
-		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s%s\n", currentLineContents, parts[i])
-			currentLineContents = ""
-		}
-		currentLineContents += parts[len(parts)-1]
-	}
+	}()
+	
+	return lines
 }
