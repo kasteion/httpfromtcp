e7aea8e4441b150ae8dbc0e2e6df37ec873e985b
Fredy Augusto Castellon Bedoya <castellonbedoya@gmail.com>
2026-03-02 12:07:01 -0600

    CH7: HTTP Responses - Server

---
+++ internal/server/server.go
@@ -0,0 +1,55 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync/atomic"
+)
+
+type Server struct {
+	closed atomic.Bool
+	listener net.Listener
+}
+
+func Serve(port int) (*Server, error) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return nil, err
+	}
+	server := &Server{ listener: listener }
+	go server.listen()
+	return server, nil
+}
+
+func (s *Server) Close() error {
+	s.closed.Store(true)
+	if s.listener != nil {
+		return s.listener.Close()
+	}
+	return nil
+}
+
+func (s *Server) listen() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			if s.closed.Load() {
+				return
+			}
+			log.Printf("could not accept connection: %s", err)
+			continue
+		}
+		go s.handle(conn)
+	}
+}
+
+func (s *Server) handle(conn net.Conn) {
+	defer conn.Close()
+	response := "HTTP/1.1 200 OK\r\n" + // Status line
+		"Content-Type: text/plain\r\n" + // Example header
+		"Content-Length: 13\r\n" + // Content length header
+		"\r\n" + // Blank line to separate headers from the body
+		"Hello World!\n" // Body
+	conn.Write([]byte(response))
+}
